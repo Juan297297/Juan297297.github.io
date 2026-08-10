43d23f192b0f4f65381bbfaf768f4b2ce0e7b5af
Juan297297 <jepalaciosv@unbosque.edu.co>
2026-08-09 22:38:15 -0500

    Fix formatting and update sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,31 @@
 // @ts-check
 import { defineConfig, fontProviders } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   fonts: [
-    { 
+    {
       provider: fontProviders.fontsource(),
-      name: "VT323",
-      cssVariable: "--font-pixels",
-      fallbacks: ["monospace"],
-      styles: ["normal", "italic"],
+      name: 'VT323',
+      cssVariable: '--font-pixels',
+      fallbacks: ['monospace'],
+      styles: ['normal', 'italic'],
     },
-    { 
+    {
       provider: fontProviders.fontsource(),
-      name: "JetBrains Mono",
-      cssVariable: "--font-mono",
-      fallbacks: ["monospace"],
+      name: 'JetBrains Mono',
+      cssVariable: '--font-mono',
+      fallbacks: ['monospace'],
       weights: [400, 500, 600, 700, 800],
-      styles: ["normal", "italic"],
-    }
+      styles: ['normal', 'italic'],
+    },
   ],
 
   vite: {
-    plugins: [tailwindcss)]
+    plugins: [tailwindcss()],
   },
-  site: "https://Juan297297.github.io",
-  integrations: [sitemap)]
+  site: 'https://Juan297297.github.io',
+  integrations: [sitemap()],
 });
